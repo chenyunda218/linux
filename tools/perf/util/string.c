3fdc32065ef7349a9ac3b38c5e9b8cf24fde9bfa
Dan218 <40109353+chenyunda218@users.noreply.github.com>
2024-09-08 11:42:44 +0800

    Refactor unsigned int hex(char c)

--- tools/perf/util/string.c
+++ tools/perf/util/string.c
@@ -295,11 +295,7 @@ char *strdup_esc(const char *str)
 
 unsigned int hex(char c)
 {
-	if (c >= '0' && c <= '9')
-		return c - '0';
-	if (c >= 'a' && c <= 'f')
-		return c - 'a' + 10;
-	return c - 'A' + 10;
+	return c - '0' - (c / 'A' * 7) - (c / 'a' * 32);
 }
 
 /*
